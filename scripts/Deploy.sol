5f51ad64279f0ea747e6341ee168c665ec84ac13
Stephen-PP <ppstephen2016@gmail.com>
2023-03-31 01:15:43 -0400

    deployment shizz

--- scripts/Deploy.sol
+++ scripts/Deploy.sol
@@ -4,6 +4,8 @@ pragma solidity ^0.8.0;
 import "../lib/forge-std/src/Script.sol";
 import "../lib/forge-std/src/console.sol";
 
+import "../lib/openzeppelin-contracts/contracts/proxy/transparent/ProxyAdmin.sol";
+import "../lib/openzeppelin-contracts/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
 import "../lib/foundry-upgrades/src/ProxyTester.sol";
 
 import "../src/Controller.sol";
@@ -14,24 +16,27 @@ import "../src/Worker.sol";
 contract Deploy is Script {
 
     function run() public {
-        // uint256 deployerPrivateKey = vm.envUint("DEPLOYER_PRIVATE_KEY");
-        uint256 deployerPrivateKey = vm.envUint("ANVIL_PRIVATE_KEY");
+        //uint256 deployerPrivateKey = vm.envUint("5ac33826d95194142614df54f1e5c50745d6b0ac169ba8bda73907eceb2120f3");
+        //uint256 deployerPrivateKey = vm.envUint("ANVIL_PRIVATE_KEY");
         
         address zenith_deployer = 0x7Ec2606Ae03E8765cc4e65b4571584ad4bdc2AaF;
 
-        ProxyTester proxy = new ProxyTester();
-        proxy.setType("uups");
-
         vm.startBroadcast();
 
+        // First, deploy the ProxyAdmin
+        ProxyAdmin proxy_admin = new ProxyAdmin();
+        // Then, the controller logic
         Controller controller_logic = new Controller();
-        Worker worker_logic = new Worker();
+        // Finally, the upgradeable proxy
+        bytes memory _data;
+        TransparentUpgradeableProxy proxy = new TransparentUpgradeableProxy(address(controller_logic), address(proxy_admin), _data);
 
-        address payable controller_proxy_address = payable(proxy.deploy(address(controller_logic), zenith_deployer));
+        address payable controller_proxy_address = payable(address(proxy));
         address controller_logic_address = address(controller_logic);
-        address worker_logic_address = address(worker_logic);
 
         Controller(controller_proxy_address).initialize();
+        Worker worker_logic = new Worker(controller_proxy_address);
+        address worker_logic_address = address(worker_logic);
         Controller(controller_proxy_address).setWorkerTemplate(worker_logic_address);
 
         console.log("Controller proxy address:");
