bfaeb06e67e141de8df303a4bd59ecc519cd0370
Stephen-PP <ppstephen2016@gmail.com>
2023-04-14 18:40:04 -0400

    benchmark not workin!

--- test/benchmarks/Benchmarks.sol
+++ test/benchmarks/Benchmarks.sol
@@ -1,2 +1,40 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.19;
+
+import "../setup/Setup.sol";
+import "../setup/ControllerSetup.sol";
+import "../setup/BenchmarkSetup.sol";
+
+interface NFT {
+    function publicMint(uint256) external;
+    function ownerOf(uint256) external returns(address);
+}
+
+contract Benchmarks is Test, Setup, ControllerSetup, BenchmarkSetup {
+
+    function setUp() external{
+        //_forkMainnet();
+        _deployController();
+    }
+
+    // Comparing to Katana V2 Hayaoki mint (25 units - 1,998,743 gas)
+    function test_hayaokiMint() external {
+        // Set the stage for our mint
+        //vm.rollFork(16922864);
+
+        // Set up the mint state
+        address nft = 0x3399B6e00b350b226AA18D3D552D750c326Ee475;
+        vm.startPrank(0xd22751a2b759d47993A6bDC466ECEfa0BdDCaF0a);
+        nft.call(abi.encodeWithSignature("setState(uint8)", 2));
+        nft.call(abi.encodeWithSignature("setNoCost(uint256)", 1));
+        nft.call(abi.encodeWithSignature("setNoCostLimit(uint256)", 800));
+        nft.call(abi.encodeWithSignature("setSalePrice(uint256)", 3000000000000000));
+        nft.call(abi.encodeWithSignature("setMaxTx(uint256)", 20));
+        vm.stopPrank();
+
+        // Execute our mint
+        vm.prank(test_user);
+        controller.callWorkers(nft, abi.encodeWithSignature("publicMint(uint256)", 1), 0, 25, 0, true);
+    }
+
+}
--- test/setup/BenchmarkSetup.sol
+++ test/setup/BenchmarkSetup.sol
@@ -1,2 +1,22 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
+
+import "../../lib/forge-std/src/Test.sol";
+
+import "../../src/Controller.sol";
+import "../../src/Worker.sol";
+import "../../lib/solady/src/utils/ERC1967Factory.sol";
+
+contract BenchmarkSetup is Test{
+
+    function _forkMainnet() internal {
+        // Add our Etherscan API key to env
+        vm.setEnv("ETHERSCAN_API_KEY", "2WCM87KHSJXKWN6YN74UFUFU9CFHKN7CHH");
+
+        // Create the fork
+        uint256 forkId = vm.createFork("https://eth-mainnet.g.alchemy.com/v2/0OIPHqbatN-7jVKMVSJ5wv7NJKSX4Mlw");
+        // Now, update vm to be using it
+        vm.selectFork(forkId);
+    }
+
+}
