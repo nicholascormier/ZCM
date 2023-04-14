fa85506073317991e55079e55995a722b7bdeb17
Stephen-PP <ppstephen2016@gmail.com>
2023-04-14 17:40:07 -0400

    fully updated to 0.8.19

--- src/Controller.sol
+++ src/Controller.sol
@@ -1,5 +1,5 @@
 //SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.19;
 
 import "../lib/openzeppelin-contracts-upgradeable/contracts/access/OwnableUpgradeable.sol";
 import "../lib/openzeppelin-contracts-upgradeable/contracts/proxy/utils/Initializable.sol";
